32c71d7c431077f741c36e42fc06e04205a41ad4
ArchangeL <dhunter24091992@gmail.com>
2014-05-14 22:48:12 +0300

    start with GUI

---
+++ Diplom/Assets/Scripts/UI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour
+{
+	public float heightCoeficient = 0f;
+	public float widthCoeficient = 0f;
+
+	public static Rect exitButton;
+	public static Rect bustButton;
+	
+
+	void Start ()
+	{
+		heightCoeficient = 444f / Screen.height ;
+		widthCoeficient = 1040f / Screen.width;
+
+		exitButton = new Rect(10 * widthCoeficient,10 * heightCoeficient,200 * widthCoeficient,50 * heightCoeficient);
+		bustButton = new Rect(Screen.width / 2 * widthCoeficient,10 * heightCoeficient,50 * widthCoeficient,50 * heightCoeficient);
+	}
+	
+	void Update ()
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(exitButton),"Залишити програму"))
+		{
+			Application.Quit();
+		}
+		if(GUI.Button(new Rect(bustButton),"GO!"))
+		{
+			Debug.Log("Burning Out!!!");
+		}
+	}
+}
