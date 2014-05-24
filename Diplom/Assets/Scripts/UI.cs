890ee88970f8fe530528e4636e97e14f4bdce3e3
ArchangeL <dhunter24091992@gmail.com>
2014-05-24 12:45:18 +0300

    added ship info

--- Diplom/Assets/Scripts/UI.cs
+++ Diplom/Assets/Scripts/UI.cs
@@ -11,9 +11,9 @@ public class UI : MonoBehaviour
 	Rect exitButton;
 	Rect bustButton;
 	Rect planetInfo;
+	Rect sheepInfo;
 	Rect flyButton;
 	Rect endFlyMessage;
-
 	string Name;
 	float Weight;
 	float Radius;
@@ -22,16 +22,14 @@ public class UI : MonoBehaviour
 	float declY;
 	float Eks;
 	float Period;
-
 	public GameObject ship;
 	bool isNitro = false;
 	bool isFlyEnded = false;
-
 	public float MAccs = 0.1f;
 	public float MaxAccs = 0.7f;
+	public GUIStyle style = new GUIStyle ();
 
-	public GUIStyle style = new GUIStyle();
-	void Awake()
+	void Awake ()
 	{
 
 	}
@@ -40,16 +38,17 @@ public class UI : MonoBehaviour
 	{
 		heightCoeficient = 444f / Screen.height;
 		widthCoeficient = 1040f / Screen.width;
-		ship = GameObject.FindGameObjectWithTag("StarDestroyer");
+		ship = GameObject.FindGameObjectWithTag ("StarDestroyer");
 		style.alignment = TextAnchor.MiddleCenter;
 		style.normal.textColor = Color.white;
 
 
 		exitButton = new Rect (10 * widthCoeficient, 10 * heightCoeficient, 200 * widthCoeficient, 50 * heightCoeficient);
-		bustButton = new Rect ((Screen.width / 2 - 25)* widthCoeficient, 10 * heightCoeficient, 50 * widthCoeficient, 50 * heightCoeficient);
+		bustButton = new Rect ((Screen.width / 2 - 25) * widthCoeficient, 10 * heightCoeficient, 50 * widthCoeficient, 50 * heightCoeficient);
 		flyButton = new Rect (10 * widthCoeficient, (Screen.height - 50) * heightCoeficient, 100 * widthCoeficient, 100 * heightCoeficient);
 		planetInfo = new Rect ((Screen.width - (Screen.width / 3.7f) + 10) * widthCoeficient, 10 * heightCoeficient, (Screen.width / 4) * widthCoeficient, 200 * heightCoeficient);
-		endFlyMessage = new Rect ((Screen.width / 2 - 100) * widthCoeficient,(Screen.height / 1.07f - 50) * heightCoeficient, 200 * widthCoeficient, 100 * heightCoeficient);
+		sheepInfo = new Rect ((Screen.width - (Screen.width / 3.7f) + 10) * widthCoeficient, planetInfo.height + (50 * heightCoeficient), (Screen.width / 4) * widthCoeficient, 100 * heightCoeficient);
+		endFlyMessage = new Rect ((Screen.width / 2 - 100) * widthCoeficient, (Screen.height / 1.07f - 50) * heightCoeficient, 200 * widthCoeficient, 100 * heightCoeficient);
 	}
 	
 	void Update ()
@@ -57,13 +56,14 @@ public class UI : MonoBehaviour
 		GetPlanetInfo ();
 	}
 
-	void FixedUpdate()
+	void FixedUpdate ()
 	{
 		exitButton = new Rect (10 * widthCoeficient, 10 * heightCoeficient, 200 * widthCoeficient, 50 * heightCoeficient);
-		bustButton = new Rect ((Screen.width / 2 - 25)* widthCoeficient, 10 * heightCoeficient, 50 * widthCoeficient, 50 * heightCoeficient);
+		bustButton = new Rect ((Screen.width / 2 - 25) * widthCoeficient, 10 * heightCoeficient, 50 * widthCoeficient, 50 * heightCoeficient);
 		flyButton = new Rect (10 * widthCoeficient, (Screen.height - 50) * heightCoeficient, 100 * widthCoeficient, 100 * heightCoeficient);
 		planetInfo = new Rect ((Screen.width - (Screen.width / 3.7f) + 10) * widthCoeficient, 10 * heightCoeficient, (Screen.width / 4) * widthCoeficient, 200 * heightCoeficient);
-		endFlyMessage = new Rect ((Screen.width / 2 - 100) * widthCoeficient,(Screen.height / 1.07f - 50) * heightCoeficient, 200 * widthCoeficient, 100 * heightCoeficient);
+		sheepInfo = new Rect ((Screen.width - (Screen.width / 3.7f) + 10) * widthCoeficient, planetInfo.height + (50 * heightCoeficient), (Screen.width / 4) * widthCoeficient, 100 * heightCoeficient);
+		endFlyMessage = new Rect ((Screen.width / 2 - 100) * widthCoeficient, (Screen.height / 1.07f - 50) * heightCoeficient, 200 * widthCoeficient, 100 * heightCoeficient);
 	}
 
 	void OnGUI ()
@@ -73,13 +73,13 @@ public class UI : MonoBehaviour
 		}
 
 		if (GUI.Button (new Rect (bustButton), "GO!")) {
-			if ((ship.GetComponent<SpaceShipPhysics>().Acceleration + MAccs) < MaxAccs)
-				ship.GetComponent<SpaceShipPhysics>().Acceleration += MAccs;
+			if ((ship.GetComponent<SpaceShipPhysics> ().Acceleration + MAccs) < MaxAccs)
+				ship.GetComponent<SpaceShipPhysics> ().Acceleration += MAccs;
 		}
 
 		GUI.TextArea (new Rect (planetInfo), 
 		    " Назва планети: " + Name +
-		    "\n Вага планети: " + Weight +
+			"\n Вага планети: " + Weight +
 			"\n Радіус: " + Radius + 
 			"\n Нахил орбіти по осі Х: " + declX + 
 			"\n Нахил орбіти по осі Y: " + declY +
@@ -87,43 +87,44 @@ public class UI : MonoBehaviour
 			"\n Ексцентриситет орбіти: " + Eks + 
 			"\n Період обертання: " + Period);
 
+		GUI.TextArea (new Rect(sheepInfo),
+		              " Швидкість корабля (а.о./день): " + ship.GetComponent<SpaceShipPhysics>().UICurrentSpeed + 
+		              "\n Пройдений шлях (а.о.): " + ship.GetComponent<SpaceShipPhysics>().UIPathLength + 
+		              "\n Час польоту (дні): " + ship.GetComponent<SpaceShipPhysics>().UIFlyTimeCon);
+
 		if (GUI.Button (new Rect (flyButton), "Вилетіти")) {
-			ship.GetComponent<SpaceShipPhysics>().SdLaunched = true;
-			ship.GetComponent<SpaceShipPhysics>().DestPlanet = GameObject.Find(planet.name);//!!!
-			ship.GetComponent<SpaceShipPhysics>().Acceleration += 0.3f; // start acceleration
+			ship.GetComponent<SpaceShipPhysics> ().SdLaunched = true;
+			ship.GetComponent<SpaceShipPhysics> ().DestPlanet = GameObject.Find (planet.name);//!!!
+			ship.GetComponent<SpaceShipPhysics> ().Acceleration += 0.3f; // start acceleration
 		}
 
-		if (ship.GetComponent<SpaceShipPhysics>().SdArrived) 
-		{
-			GUI.TextField(endFlyMessage,"Корабель прилетів до планети!",style);
+		if (ship.GetComponent<SpaceShipPhysics> ().SdArrived) {
+			GUI.TextField (endFlyMessage, "Корабель прилетів до планети!", style);
 		}
 
 	}
 
 	void GetPlanetInfo ()
 	{
-		try
-		{
-		planet = Camera.main.GetComponent<CameraController>().OrbitTarget;
-		data = planet.GetComponent<PlanetMovement>();
-		Name = planet.name;
-		Weight = data.Wheight;
-		Radius = data.Radius;
-		declX = data.declX;
-		declY = data.declY;
-		declZ = data.declZ;
-		Eks = data.Eks;
-		Period = data.Period;
-		}
-		catch(System.Exception e)
-		{
+		try {
+			planet = Camera.main.GetComponent<CameraController> ().OrbitTarget;
+			data = planet.GetComponent<PlanetMovement> ();
+			Name = planet.name;
+			Weight = data.Wheight;
+			Radius = data.Radius;
+			declX = data.declX;
+			declY = data.declY;
+			declZ = data.declZ;
+			Eks = data.Eks;
+			Period = data.Period;
+		} catch (System.Exception e) {
 		}
 	}
 
-	void GetPlanetsForFly()
+	void GetPlanetsForFly ()
 	{
 		GameObject[] planets;
-		planets = ship.GetComponent<SpaceShipPhysics>().Planets;
+		planets = ship.GetComponent<SpaceShipPhysics> ().Planets;
 	
 	}
 }
